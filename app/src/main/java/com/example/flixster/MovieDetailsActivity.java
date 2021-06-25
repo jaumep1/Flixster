16f36a9211eb1b932ce857602f68d120cac65687
jaumep1 <76637874+jaumep1@users.noreply.github.com>
2021-06-25 15:12:54 -0700

    Moved trailer video key client call to MovieDetailsActivity.onCreate()

--- app/src/main/java/com/example/flixster/MovieDetailsActivity.java
+++ app/src/main/java/com/example/flixster/MovieDetailsActivity.java
@@ -10,16 +10,29 @@ import android.view.View;
 import android.widget.RelativeLayout;
 
 import com.bumptech.glide.Glide;
+import com.codepath.asynchttpclient.AsyncHttpClient;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
 import com.example.flixster.databinding.ActivityMovieDetailsBinding;
 import com.example.flixster.models.Movie;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.parceler.Parcels;
 
+import java.util.Locale;
+
 import jp.wasabeef.glide.transformations.RoundedCornersTransformation;
+import okhttp3.Headers;
 
 public class MovieDetailsActivity extends AppCompatActivity implements View.OnClickListener {
 
+    public static final String VIDEOS = "https://api.themoviedb.org/3/movie/%d/videos?api_key=%s";
+    public static final String TAG = "MovieDetailsActivity";
+
+
     Movie movie;
+    String key;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +45,7 @@ public class MovieDetailsActivity extends AppCompatActivity implements View.OnCl
         setContentView(binding.getRoot());
 
         //Unwrap intent parcel
-        movie = (Movie) Parcels.unwrap(getIntent().getParcelableExtra(Movie.class.getSimpleName()));
+        movie = Parcels.unwrap(getIntent().getParcelableExtra(Movie.class.getSimpleName()));
         Log.d("MovieDetailsActivity",
                 String.format("Showing details for %s", movie.getTitle()));
 
@@ -69,6 +82,38 @@ public class MovieDetailsActivity extends AppCompatActivity implements View.OnCl
                 .into(binding.ivPoster);
 
         binding.ivPoster.setOnClickListener(this);
+
+        //Get video key for displaying trailer
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(String.format(VIDEOS, movie.getId(), getString(R.string.theMovieDBKey)),
+                new JsonHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int i, Headers headers, JSON json) {
+                        Log.d(TAG, "onSuccess");
+                        JSONObject jsonObject = json.jsonObject;
+                        try {
+                            JSONArray results = jsonObject.getJSONArray("results");
+                            Log.i(TAG, "Results: " + results.toString());
+
+                            //Make sure link is a YouTube link
+                            int index = 0;
+                            while (!results.getJSONObject(index)
+                                    .getString("site").equals("YouTube")) {
+                                index++;
+                            }
+                            key = results.getJSONObject(index).getString("key");
+                            Log.i(TAG, "Video Key: " + key);
+
+                        } catch (JSONException e) {
+                            Log.e(TAG, "Hit json exception", e);
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(int i, Headers headers, String s, Throwable throwable) {
+                        Log.d("Movie", "onFailure");
+                    }
+                });
     }
 
     @Override
@@ -77,10 +122,9 @@ public class MovieDetailsActivity extends AppCompatActivity implements View.OnCl
 
         //Create intent
         Intent intent = new Intent(this, MovieTrailerActivity.class);
-        intent.putExtra("vidID", movie.getKey());
+        intent.putExtra("vidID", key);
 
         //Show activity
         this.startActivity(intent);
-
     }
 }
